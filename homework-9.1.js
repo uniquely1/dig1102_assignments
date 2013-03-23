ab80b2d4cbabbd7dbdde8e57dc252acaa0222aae
Elaine <uniquely@cfl.rr.com>
2013-03-22 23:05:26 -0400

    finished the mulitplication function and tests had some errors due to leaving out the multiplication sign in function

--- homework-9.1.js
+++ homework-9.1.js
@@ -3,7 +3,7 @@ var assert = require("assert")
 //assert(1 / 1 == 1, '1 / 1 does not equal 1');
 // assert.equal(1, 1 / 1, '1 / 1 does not equal 1');
 
-/**
+/*
  * four functions with two parameters:
  * - addition
  * - subtraction
@@ -13,35 +13,38 @@ var assert = require("assert")
  * Must accept positive and negative Numbers (int or 
  * float) AND zero. Must not make an error!
 */
-
-/**
- * testing division();
+ 
+ /*
+ *testing multiplication():
  * one | two | out
  * ===============
- *  +1 | +1 | 1
- *   0 | +1 | 0
- *  +1 | -1 |-1
- *  +2 | +1 |+2
- *  +2 | -1 |-2
- *   0 | +2 | 0
- *  -2 | +2 |-1
- *  +2 | -2 |-1
- *  +2 | +2 |+1
+ * +1 | +1 | +1
+ * +1 | +0 | +0
+ * +1 | -1 | -1
+ * -1 | -1 | +1
+ * +2 | +2 | +4
+ * +2 | +0 | +0
+ * +2 | -2 | -4
+ * -2 | -2 | +4
+ * +0 | +0 | +0
  */
  
- //This is a division function
- var division = function(a, b) {
-     return (a/b);
+// This is a multiplication function
+var multiplication = function(a, b) {
+    return (a*b);
 }
+ // These are multiplication tests
+assert.equal(1,multiplication(1,1));
+assert.equal(0,multiplication(1,0));
+assert.equal(-1,multiplication(1,-1));
+assert.equal(1,multiplication(-1,-1));
+assert.equal(4,multiplication(2,2));
+assert.equal(0,multiplication(2,0));
+assert.equal(-4,multiplication(2,-2));
+assert.equal(4,multiplication(-2,-2));
+assert.equal(0,multiplication(0,0));
+ 
+  
 
- //These are division tests
- assert.equal(1,division(1,1));
- assert.equal(0,division(0,1));
- assert.equal(-1,division(1,-1));
- assert.equal(2,division(2,1));
- assert.equal(-2,division(2,-1));
- assert.equal(0,division(0,2));
- assert.equal(-1,division(-2,2));
- assert.equal(-1,division(2,-2));
- assert.equal(1,division(2,2));
+ 
  
