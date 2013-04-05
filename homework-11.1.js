1fb8191c5fbe8839272997fcf12a9af0665a8c3d
Elaine <uniquely@cfl.rr.com>
2013-04-04 23:50:53 -0400

    having trouble with cloud nine; if I backspace while writing code I can't type any more code without exiting and signing back in

---
+++ homework-11.1.js
@@ -0,0 +1,21 @@
+var assert = require("assert");
+
+/**
+ * four functions with two parameters
+ * addition
+ * subtraction
+ * multiplication
+ * division
+ * 
+ * Must accept positive and negative numbers (int and float)
+ * and zero. Must not return an error!
+ */
+ 
+ /**
+  * testing multiplication():
+  * one | two | out
+  * +0 | +0 | +0
+  * +0 | +1 | +0
+  * +0 | -1 | +0
+  * +1 | +1 | +1
+  * +1 |-1 | -1
