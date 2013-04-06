090e9470f87c9622f61902f41b54506fe1a891ba
Elaine <uniquely@cfl.rr.com>
2013-04-06 00:20:47 -0400

    first multiplicattion function passed three out of four tests; refactored function; passed three of four tests; need to refactor function

--- homework-11.1.js
+++ homework-11.1.js
@@ -26,5 +26,24 @@ var assert = require("assert");
   */
   
  //first multiplication test
- assert( multiplication(+0, +0) == +1, '+0 * +0 == +1');
+ //assert( multiplication(+0, +0) == +1, '+0 * +0 == +1');
+ // test failed
  
+ // multiplication function
+//function multiplication(a, b){
+//    return (+0);
+//}
+//first test after writing function
+assert( multiplication(+0, +0) == +0, '+0 * +0 == +0');
+assert( multiplication(+0, +1) == +0, '+0 * +1 == +0');
+assert( multiplication(+0, -1) == +0, '+0 * -1 == +0');
+assert( multiplication(+1, +1) == +1, '+1 * +1 == +1');
+//first three tests passed; last test failed; need to refactor function to get a 
+//passing test
+
+//refactored multiplication test
+function multiplication(a, b){
+    if (a || b == +0) return +0;
+    if (a == +1) return +1;
+}
+
