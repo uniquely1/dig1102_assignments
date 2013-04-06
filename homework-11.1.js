c80b80bf1da3a509d8c2c799f35bc4ac4eef9284
Elaine <uniquely@cfl.rr.com>
2013-04-06 08:23:08 -0400

    quitting now; having trouble getting last test to pass by taking baby steps ; been a frustrating week on cloud nine

--- homework-11.1.js
+++ homework-11.1.js
@@ -44,6 +44,10 @@ assert( multiplication(+1, +1) == +1, '+1 * +1 == +1');
 //refactored multiplication test
 function multiplication(a, b){
     if (a || b == +0) return +0;
-    if (a == +1) return +1;
+    if (b == +1) return +0;
+    if (b == -1) return +0;
+    if (a == +1 && b == +1) return +1;
 }
+//still not passing last test
+
 
